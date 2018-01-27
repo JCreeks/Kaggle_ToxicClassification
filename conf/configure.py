b9a0782a145c260487caebf31ed74b421c394afd
Ubuntu <ubuntu@ip-172-31-3-128.us-east-2.compute.internal>
2018-01-27 15:59:05 +0000

    add further utils

---
+++ conf/configure.py
@@ -0,0 +1,38 @@
+#!/usr/local/miniconda2/bin/python
+# _*_ coding: utf-8 _*_
+
+"""
+@author: Jing Guo
+@time  : 12/26/17
+"""
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('..'))
+sys.path.append(module_path)
+
+import time
+
+
+class Configure(object):
+
+    model_path = '../model'
+    train_data_path = '../input/train.csv'
+    test_data_path = '../input/test.csv'
+    
+    embedding_path = '../input/crawl-300d-2M.vec'
+    
+    cleaned_x_train_path = '../input/cleaned_x_train.pkl'
+    cleaned_y_train_path = '../input/cleaned_y_train.pkl'
+    
+    processed_train_path = '../input/train_dataset.pkl'
+    processed_test_path = '../input/test_dataset.pkl'
+    
+    x_train_path = '../input/x_train.pkl'
+    y_train_path = '../input/y_train.pkl'
+    x_test_path = '../input/x_test.pkl'
+    test_fname_path = '../input/test_fname.pkl'
+    
+    outlierNameList = '../input/outlierNameList.pkl'
+
+    submission_path = '../output/submission_{}.csv'.format(time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time())))
