3d2c553f0c8235a77b0fbf6b618cf922aee11756
Ubuntu <ubuntu@ip-172-31-3-128.us-east-2.compute.internal>
2018-02-01 09:15:03 +0000

    add CNN_baseline

---
+++ models/CNN_baseline.py
@@ -0,0 +1,103 @@
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('..'))
+sys.path.append(module_path)
+
+from conf.configure import Configure as conf
+from utils import data_util
+from utils.data_util import max_len
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "../input"]).decode("utf8"))
+
+# Any results you write to the current directory are saved as output.
+
+from keras.models import Model
+from keras.models import load_model
+from keras.layers import Dense, Embedding, Input
+from keras.layers import LSTM, Bidirectional, GlobalMaxPool1D, Dropout, CuDNNGRU, GRU
+from keras.preprocessing import text, sequence
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Bidirectional, Dropout
+from keras.models import Model
+from keras.optimizers import RMSprop
+from keras.layers import Input, Dense, Embedding, Conv2D, MaxPool2D
+from keras.layers import Reshape, Flatten, Dropout, Concatenate
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+from keras.models import Model
+import gc
+
+from keras import backend as K
+import tensorflow as tf
+n_jobs=5
+config = tf.ConfigProto(intra_op_parallelism_threads=n_jobs, 
+                        inter_op_parallelism_threads=n_jobs, 
+                        allow_soft_placement=True, 
+                        device_count = {'GPU': n_jobs})
+session = tf.Session(config=config)
+K.set_session(session)
+
+maxlen = max_len()
+
+list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+x_train, y_train, x_test = data_util.load_dataset()
+
+embedding_matrix = data_util.load_embedding_matrix()
+sequence_length = maxlen # 500
+vocabulary_size = embedding_matrix.shape[0]
+embedding_dim = embedding_matrix.shape[1]
+filter_sizes = [3,4,5]
+num_filters = 512
+drop = 0.5
+
+epochs = 2
+batch_size = 30
+
+del embedding_matrix
+gc.collect()
+
+# this returns a tensor
+print("Creating Model...")
+inputs = Input(shape=(sequence_length,), dtype='int32')
+embedding = Embedding(input_dim=vocabulary_size, output_dim=embedding_dim, input_length=sequence_length)(inputs)
+reshape = Reshape((sequence_length,embedding_dim,1))(embedding)
+
+conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+
+maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
+dropout = Dropout(drop)(flatten)
+output = Dense(units=y_train.shape[1], activation='softmax')(dropout)
+
+# this creates a model that includes
+model = Model(inputs=inputs, outputs=output)
+
+checkpoint = ModelCheckpoint('checkpoints/'+'weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
+adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+print("Traning Model...")
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[checkpoint], validation_split=0.1)  # starts training
+
+print("Start predicting")
+y_test = model.predict(x_test)
+
+sample_submission = pd.read_csv("../input/sample_submission.csv")
+
+sample_submission[list_classes] = y_test
+
+sample_submission.to_csv(conf.submission_path, index=False)
