6211ea26bc6b0ab168ecd99bba6af151658f89ce
Ubuntu <ubuntu@ip-172-31-3-128.us-east-2.compute.internal>
2018-03-17 21:21:57 +0000

    update run.sh

--- ensemble/run.sh
+++ ensemble/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 nohup python Pooled_GRU_kFold.py > pooled_gru.out
 nohup python BiLSTM_CNN_kFold.py > bilstm_cnn.out
-python BiGRU_CNN_kFold.py > bigru_cnn.out
-python LSTM_clean_kFold.py > lstm.out
-python GRU_kFold.py > gru.out
+nohup python BiGRU_CNN_kFold.py > bigru_cnn.out
+nohup python LSTM_clean_kFold.py > lstm.out
+nohup python GRU_kFold.py > gru.out
